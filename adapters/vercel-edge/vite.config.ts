37fc231436e13a7f98617331d3c4fe884850db56
santicasalis <santicasalis@hotmail.com>
2024-03-06 10:59:31 -0300

    deploy

--- adapters/vercel-edge/vite.config.ts
+++ adapters/vercel-edge/vite.config.ts
@@ -5,10 +5,10 @@ import baseConfig from "../../vite.config";
 export default extendConfig(baseConfig, () => {
   return {
     build: {
-      ssr: false,
+      ssr: true,
       rollupOptions: {
         input: ["src/entry.vercel-edge.tsx", "@qwik-city-plan"],
-        external: ['async_hooks'], 
+       
       },
       outDir: ".vercel/output/functions/_qwik-city.func",
     },
